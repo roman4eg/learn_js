06e231a5e076c7ef29fad447b78de20291ab6a4d
Roman <paluchaeca@gmail.com>
2022-02-03 21:03:31 +0200

    task2

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,13 @@ let nubmerOfFilms;
 nubmerOfFilms = prompt( "Сколько фильмов вы уже посмотрели?", 0);
 
 console.log(nubmerOfFilms);
+
+let personalMovieDB = {
+    count: nubmerOfFilms,
+    movies: {},
+    actors: {},
+    genres: "",
+    privat: false,
+};
+
+console.log(typeof(personalMovieDB.genres));
